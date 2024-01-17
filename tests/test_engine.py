25e019626222d2735a73fad73d30d65fb9067735
yli150 <victor.y.li@intel.com>
2024-01-17 20:40:02 +0800

    EngineMulStage

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,5 +1,6 @@
 import unittest
 from perfsim.engine.enginebase import EngineBase
+from perfsim.engine.enginemulstage import EngineMulStage
 from perfsim.context.simcontext import SimContext
 from perfsim.common.command import RequestCmd
 
@@ -17,6 +18,17 @@ class TestEngine(unittest.TestCase):
             ctx.attach(hw)
 
         jobs_num = 16
+        cmds = [RequestCmd(f'wl_{i}', 'compute', i) for i in range(jobs_num)]
+        ctx.process(cmds)
+        env.run()
+
+    def test_multiple_stage_engine(self):
+        env = simpy.Environment()
+        ctx = SimContext(env)
+        hw = EngineMulStage(env, 'hw')
+        ctx.attach(hw)
+
+        jobs_num = 1
         cmds = [RequestCmd(f'wl_{i}', 'compute', i) for i in range(jobs_num)]
         ctx.process(cmds)
         env.run()
