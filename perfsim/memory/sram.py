bcf3b3a2974e6aed6dd04a9f5202a5d8afd75ec1
yli150 <victor.y.li@intel.com>
2024-01-22 21:31:45 +0800

    introduce packet

--- perfsim/memory/sram.py
+++ perfsim/memory/sram.py
@@ -5,11 +5,13 @@ from perfsim.barrier.barriermgr import BarrierMgr
 from perfsim.context.context import Context
 import simpy
 from typing import List
+from perfsim.common.packet import StatisticPacket
 
 
 class SRAM(Memory):
     def __init__(self, context: Context, name: str) -> None:
         super().__init__(context, name)
+        self.device_id = 1000
 
     def post_init(self):
         super().post_init()
@@ -24,9 +26,9 @@ class SRAM(Memory):
 
     def request(self, memCmd):
         if memCmd.type == MemOp.READ:
-            yield self.readQ.put(memCmd)
+            yield self.readQ.put(StatisticPacket(memCmd, self.device_id))
         if memCmd.type == MemOp.WRITE:
-            yield self.writeQ.put(memCmd)
+            yield self.writeQ.put(StatisticPacket(memCmd, self.device_id))
 
     def read(self):
         while True:
@@ -36,9 +38,12 @@ class SRAM(Memory):
             barrier_wait_for = [self.barrierMgr.get(b).producer_event for b in rdcmd.cdeps]
             yield simpy.AllOf(self.env, barrier_wait_for)
 
+            rdcmd.start(self.env.now)
             latency = max(rdcmd.size, 1)
             print(f'Device {self.name} READ {rdcmd},  takes {latency} to process at {self.env.now}')
             yield self.env.timeout(latency)
+            rdcmd.terminate(self.env.now)
+
             yield self.cmd_out_queue.put(rdcmd)
 
             # release barrier
@@ -54,9 +59,14 @@ class SRAM(Memory):
             barrier_wait_for = [self.barrierMgr.get(b).producer_event for b in wrcmd.cdeps]
             yield simpy.AllOf(self.env, barrier_wait_for)
 
+            wrcmd.start(self.env.now)
+
             latency = wrcmd.size * 2
             print(f'Device {self.name} WRITE {wrcmd},  takes {latency} to process at {self.env.now}')
             yield self.env.timeout(latency)
+
+            wrcmd.terminate(self.env.now)
+
             yield self.cmd_out_queue.put(wrcmd)
 
             # release barrier
