9de416e7a13b3f0aafae71711ac31b42658e9334
yli150 <victor.y.li@intel.com>
2024-01-23 21:33:59 +0800

    fix trace export

--- perfsim/common/record.py
+++ perfsim/common/record.py
@@ -4,14 +4,14 @@ from .packet import StatisticPacket
 class Record(object):
     @classmethod
     def from_packet(cls, packet: StatisticPacket):
-        return cls(packet.id, packet.name, packet.device_id, packet.startT, packet.endT)
+        return cls(packet.id, packet.name, packet.devicedes, packet.startT, packet.endT)
 
-    def __init__(self, id: int, name: str, device_id: int, startT: int, endT: int) -> None:
+    def __init__(self, id: int, name: str, devicedes: 'DeviceDes', startT: int, endT: int) -> None:
         self.id = id
         self.name = name
-        self.device_id = device_id
+        self.devicedes = devicedes
         self.startT = startT
         self.endT = endT
 
     def __str__(self) -> str:
-        return f'Packet {self.id} Run on Device {self.device_id} from {self.startT} to {self.endT}'
+        return f'Packet {self.id} Run on Device {self.devicedes} from {self.startT} to {self.endT}'
